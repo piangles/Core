b0d078e5d3f299db4298356fe298e3bc4e1f4031
saradhivs <saradhivs@yahoo.com>
2020-11-24 04:57:09 -0500

    Added a plain MongoDB connection and a accessor method for it.

--- src/main/java/org/piangles/core/dao/nosql/AbstractDAO.java
+++ src/main/java/org/piangles/core/dao/nosql/AbstractDAO.java
@@ -1,44 +1,58 @@
 package org.piangles.core.dao.nosql;
 
+import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.piangles.core.dao.DAOException;
 import org.piangles.core.resources.MongoDataStore;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.ReplaceOptions;
 
 public abstract class AbstractDAO<T>
 {
-	private MongoCollection<T> collection = null;
+	private MongoCollection<Document> collection = null;
+	private MongoCollection<T> tCollection = null;
 
 	public void init(MongoDataStore dataStore)
 	{
-		collection = dataStore.getDatabase().getCollection(getTClass().getSimpleName(), getTClass());
+		collection = dataStore.getDatabase().getCollection(getTClass().getSimpleName());
+		tCollection = dataStore.getDatabase().getCollection(getTClass().getSimpleName(), getTClass());
 	}
 
 	protected final void create(T obj) throws DAOException
 	{
-		collection.insertOne(obj);
+		tCollection.insertOne(obj);
 	}
 	
 	protected final FindIterable<T> read(Bson filter) throws DAOException
 	{
-		return collection.find(filter);
+		return tCollection.find(filter);
 	}
 
 	protected final T readOne(Bson filter) throws DAOException
 	{
-		return (T)collection.find(filter).first();
+		return (T)tCollection.find(filter).first();
 	}
 
 	protected final void update(Bson filter, T obj) throws DAOException
 	{
-		collection.replaceOne(filter, obj);
+		tCollection.replaceOne(filter, obj);
+	}
+
+	protected final void upsert(Bson filter, T obj) throws DAOException
+	{
+		tCollection.replaceOne(filter, obj, new ReplaceOptions().upsert(true));
 	}
 
 	protected final void delete(Bson filter) throws DAOException
 	{
-		collection.deleteMany(filter);
+		tCollection.deleteMany(filter);
+	}
+	
+	protected final MongoCollection<Document> getConnection()
+	{
+		return collection;
 	}
 
 	protected abstract Class<T> getTClass();
