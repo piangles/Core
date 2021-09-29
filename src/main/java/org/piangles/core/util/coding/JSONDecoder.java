cb45d4e364a31da9dfa107d2c8cf93489cd37682
saradhivs <saradhivs@yahoo.com>
2021-09-29 15:51:17 -0400

    Incorporated Date Deserializer for JSON.

--- src/main/java/org/piangles/core/util/coding/JSONDecoder.java
+++ src/main/java/org/piangles/core/util/coding/JSONDecoder.java
@@ -20,11 +20,32 @@
 package org.piangles.core.util.coding;
 
 import java.lang.reflect.Type;
+import java.util.Date;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 
 final class JSONDecoder implements Decoder
 {
+	private GsonBuilder gsonBuilder = null;
+	
+	public JSONDecoder()
+	{
+		gsonBuilder = new GsonBuilder();
+		
+		gsonBuilder.registerTypeAdapter(Date.class, new JsonDeserializer<Date>(){
+			@Override
+			public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException
+			{
+				return new Date(json.getAsJsonPrimitive().getAsLong());
+			}
+		});
+	}
+	
 	@Override
 	public <T> T decode(byte[] data, Class<T> destClass) throws Exception
 	{
